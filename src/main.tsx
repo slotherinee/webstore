c0ffdb87574e6b242b454d672b53dd4a99858e3f
Pavel <pavelpredator6@gmail.com>
2024-02-29 17:26:43 +0300

    Refactor routing configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { store } from './store/store.ts'
 import { Provider } from 'react-redux'
-import StorePage from './pages/StorePage.tsx'
-import ErrorPage from './pages/ErrorPage.tsx'
-import MainPage from './pages/MainPage.tsx'
-import SingleItemPage from './pages/SingleItemPage.tsx'
-import Layout from './Layout.tsx'
-import CartPage from './pages/CartPage.tsx'
+import { router } from './routes/routes.tsx'
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <MainPage /> },
-      { path: '/store', element: <StorePage /> },
-      { path: '/cart', element: <CartPage /> },
-      { path: 'store/items/:id', element: <SingleItemPage /> },
-    ],
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../Layout.tsx'
+import ErrorPage from '../pages/ErrorPage.tsx'
+import MainPage from '../pages/MainPage.tsx'
+import StorePage from '../pages/StorePage.tsx'
+import CartPage from '../pages/CartPage.tsx'
+import SingleItemPage from '../pages/SingleItemPage.tsx'
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <MainPage /> },
+      { path: '/store', element: <StorePage /> },
+      { path: '/cart', element: <CartPage /> },
+      { path: 'store/items/:id', element: <SingleItemPage /> },
+    ],
+  },
+])
